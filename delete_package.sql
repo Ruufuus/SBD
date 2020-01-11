6f12df414d36fe737622bd7607565854aeb2b51c
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-01-11 15:18:28 +0100

    Update delete_package.sql

--- delete_package.sql
+++ delete_package.sql
@@ -38,6 +38,22 @@ PROCEDURE delete_studio
 (
 pname Studio.name%type
 );
+PROCEDURE delete_review
+(
+preview_date Review.Review_Date%type,
+pmedium_id Review.medium_id%type,
+pusername Review.username%type
+);
+PROCEDURE delete_user_info
+(
+pusername user_info.username%type
+);
+PROCEDURE delete_consumed_media
+(
+pconsume_date consumed_media.consume_date%type,
+pmedium_id consumed_media.medium_id%type,
+pusername consumed_media.username%type
+);
 END delete_package;
 /
 CREATE OR REPLACE PACKAGE BODY delete_package IS
@@ -133,6 +149,44 @@ IS BEGIN
 DELETE FROM Studio 
 	WHERE
 	pname = name;
+END;
+
+PROCEDURE delete_review
+(
+preview_date Review.Review_Date%type,
+pmedium_id Review.medium_id%type,
+pusername Review.username%type
+)
+IS BEGIN
+DELETE FROM REVIEW 
+	WHERE
+	preview_date=review_date and
+	pmedium_id = medium_id and
+	pusername = username;
+END;
+
+PROCEDURE delete_user_info
+(
+pusername user_info.username%type
+)
+IS BEGIN
+DELETE FROM user_info
+	WHERE
+	pusername = username;
+END;
+
+PROCEDURE delete_consumed_media
+(
+pconsume_date consumed_media.consume_date%type,
+pmedium_id consumed_media.medium_id%type,
+pusername consumed_media.username%type
+)
+IS BEGIN
+DELETE FROM consumed_media 
+	WHERE
+	pconsume_date = consume_date and 
+pmedium_id=medium_id and 
+pusername=username;
 END;
 END delete_package;
 
