925b202fb5cb6c528028fc78d132b5b52079a80b
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-01-12 18:37:41 +0100

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -56,8 +56,6 @@ CONSTRAINT episodes_number_constraint check (episodes is NULL OR episodes > 0),
 CONSTRAINT anime_type_constraint check ((type in ('ONA','OVA','TV', 'MOVIE', 'SPECIAL') OR type is NULL)),
 CONSTRAINT medium_medium_constraint check (medium in ('ANIME','MANGA','LIGHT NOVEL')),
 CONSTRAINT medium_pk PRIMARY KEY (id),
-CONSTRAINT medium_integrity_constraint check( (medium = 'ANIME' AND studio_name IS NOT NULL) OR ((medium IN ('MANGA', 'LIGHT NOVEL')
-) AND author_id IS NOT NULL) )
 );
 
 
