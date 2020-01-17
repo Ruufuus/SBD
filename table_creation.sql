987cb687c85c6c36053c31003250e1e15f480e36
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-01-17 21:17:59 +0100

    add author name and surname validation

--- table_creation.sql
+++ table_creation.sql
@@ -21,7 +21,9 @@ surname VARCHAR(32) not null,
 birthdate DATE null,
 gender VARCHAR(1) null ,
 CONSTRAINT author_gender_constraint  check ((gender in ('K', 'M') OR gender is null)),
-CONSTRAINT author_pk PRIMARY KEY (author_id)
+CONSTRAINT author_pk PRIMARY KEY (author_id),
+CONSTRAINT author_name_validation check name not like [0-9],
+CONSTRAINT author_surname_validation check surname not like [0-9]  
 );
 
 
@@ -29,7 +31,9 @@ CREATE TABLE ilustrator(
 ilustrator_id NUMBER(10) NOT NULL,
 name VARCHAR(256) NOT NULL,
 surname VARCHAR(256) NOT NULL,
-CONSTRAINT ilustrator_pk PRIMARY KEY (ilustrator_id)
+CONSTRAINT ilustrator_pk PRIMARY KEY (ilustrator_id),
+CONSTRAINT ilustrator_name_validation check name not like [0-9],
+CONSTRAINT ilustrator_surname_validation check surname not like [0-9] 
 );
 
 CREATE TABLE medium(
