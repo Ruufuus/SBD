1ff05249ca8d22282d0b630f2a0a54bc9ea3a53f
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-01-18 01:08:37 +0100

    Add errors informing about not existing records

--- delete_package.sql
+++ delete_package.sql
@@ -58,7 +58,13 @@ PROCEDURE delete_Manga
 (
 pid Medium.id%type
 )
-IS BEGIN 
+IS 
+vExist numeric;
+BEGIN 
+    select count(*) into vExist from MEDIUM where id=pid AND medium.medium='MANGA';
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrana manga nie istnieje w bazie danych!');
+    end if;
     DELETE FROM MEDIUM
 	WHERE
 	id=pid AND medium.medium='MANGA';
@@ -68,7 +74,13 @@ PROCEDURE delete_Anime
 (
 pid Medium.id%type
 )
-IS BEGIN 
+IS 
+vExist numeric;
+BEGIN 
+    select count(*) into vExist from MEDIUM where id=pid AND medium.medium='ANIME';
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrane anime nie istnieje w bazie danych!');
+    end if;
     DELETE FROM MEDIUM 
 	WHERE
 	id=pid AND medium.medium='ANIME';
@@ -78,7 +90,13 @@ PROCEDURE delete_Light_Novel
 (
 pid Medium.id%type
 )
-IS BEGIN 
+IS 
+vExist numeric;
+BEGIN 
+    select count(*) into vExist from MEDIUM where id=pid AND medium.medium='LIGHT NOVEL';
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrana Light Novel nie istnieje w bazie danych!');
+    end if;
     DELETE FROM MEDIUM 
 	where
 	id=pid AND medium.medium='LIGHT NOVEL';
@@ -88,7 +106,13 @@ PROCEDURE delete_Volume
 (
 pvolume_id Volume.volume_id%type
 )
-IS BEGIN 
+IS 
+vExist numeric;
+BEGIN 
+    select count(*) into vExist from volume WHERE volume_id=pvolume_id;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany tom nie istnieje w bazie danych!');
+    end if;
     DELETE FROM Volume 
 	WHERE
 	volume_id=pvolume_id;
@@ -98,7 +122,13 @@ PROCEDURE delete_Chapter
 (
 pchapter_id Chapter.chapter_id%type
 )
-IS BEGIN 
+IS
+vExist numeric; 
+BEGIN 
+    select count(*) into vExist from chapter WHERE chapter_id=pchapter_id;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany roździal nie istnieje w bazie danych!');
+    end if;
     DELETE FROM Chapter 
 	WHERE
 	chapter_id=pchapter_id;
@@ -108,7 +138,13 @@ PROCEDURE delete_Episode
 (
 pep_id Episode.ep_id%type
 )
-IS BEGIN 
+IS
+vExist numeric; 
+BEGIN 
+    select count(*) into vExist from Episode WHERE pep_id=ep_id;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany odcinek nie istnieje w bazie danych!');
+    end if;
     DELETE FROM Episode 
 	where
 	pep_id=ep_id;
@@ -119,7 +155,14 @@ PROCEDURE delete_Ilustrator
 (
 pilustrator_id Ilustrator.ilustrator_id%type
 )
-IS BEGIN 
+IS
+vExist numeric; 
+BEGIN 
+    select count(*) into vExist from Ilustrator  WHERE 
+	pilustrator_id=ilustrator_id;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany ilustrator nie istnieje w bazie danych!');
+    end if;
     DELETE FROM Ilustrator 
 	WHERE
 	pilustrator_id=ilustrator_id;
@@ -129,7 +172,14 @@ PROCEDURE delete_author
 (
 pauthor_id Author.author_id%type
 )
-IS BEGIN
+IS 
+vExist numeric;
+BEGIN
+select count(*) into vExist from Author  WHERE 
+	pauthor_id = author_id;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany autor nie istnieje w bazie danych!');
+    end if;
 DELETE FROM Author 
 	WHERE
 	pauthor_id = author_id;
@@ -139,7 +189,14 @@ PROCEDURE delete_studio
 (
 pname Studio.name%type
 )
-IS BEGIN
+IS 
+vExist numeric;
+BEGIN
+select count(*) into vExist from Studio  WHERE 
+	pname = name;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrane studio nie istnieje w bazie danych!');
+    end if;
 DELETE FROM Studio 
 	WHERE
 	pname = name;
@@ -151,7 +208,16 @@ preview_date Review.Review_Date%type,
 pmedium_id Review.medium_id%type,
 pusername Review.username%type
 )
-IS BEGIN
+IS 
+vExist numeric;
+BEGIN
+select count(*) into vExist from REVIEW  WHERE
+	preview_date=review_date and
+	pmedium_id = medium_id and
+	pusername = username;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrane studio nie istnieje w bazie danych!');
+    end if;
 DELETE FROM REVIEW 
 	WHERE
 	preview_date=review_date and
@@ -163,7 +229,14 @@ PROCEDURE delete_user_info
 (
 pusername user_info.username%type
 )
-IS BEGIN
+IS 
+vExist numeric;
+BEGIN
+select count(*) into vExist from user_info  WHERE
+	pusername = username;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrany użykownik nie istnieje w bazie danych!');
+    end if;
 DELETE FROM user_info
 	WHERE
 	pusername = username;
@@ -175,7 +248,15 @@ pconsume_date consumed_media.consume_date%type,
 pmedium_id consumed_media.medium_id%type,
 pusername consumed_media.username%type
 )
-IS BEGIN
+IS 
+vExist numeric;
+BEGIN
+select count(*) into vExist from consumed_media  WHERE pconsume_date = consume_date and 
+pmedium_id=medium_id and 
+pusername=username;
+    if(vExist = 0) then 
+    RAISE_APPLICATION_ERROR(-20002, 'Wybrana obejrzana pozycja nie istnieje w bazie danych!');
+    end if;
 DELETE FROM consumed_media 
 	WHERE
 	pconsume_date = consume_date and 
